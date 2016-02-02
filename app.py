72ba26ede156579c2c78494a0c412fdb2749038f
Md Islam <mdnahian@outlook.com>
2016-02-01 19:56:09 -0500

    Made Daemon

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python
 from main import Main
+from daemon import runner
 import urllib, urllib2
 import time
 import os
 
-app = Main()
 
+app = Main()
 
 def deletePYCFiles():
     directory = os.listdir('.')
@@ -21,18 +23,33 @@ def sendEmail(body, to_email="mdnahian@outlook.com", from_email="do_not_repy@mlh
     d = response.read()
     print(d)
 
-while True:
-    new_events = app.parse()
 
-    if len(new_events) > 0:
-        body = "The following hackathons have been recently posted:<br><br><ul>"
-        for event in new_events:
-            body = body + "<li><a href='" + event[3] + "'><b>" + event[0] + "</b></a> in " + event[2] + " on " + event[1] + "</li>"
-        body = body + "</ul>"
 
-        sendEmail(body)
-    else:
-        print("Everything is Up to Date")
-    
-    deletePYCFiles()
-    time.sleep(10)
+class App():
+    def __init__(self):
+        self.stdin_path = '/dev/null'
+        self.stdout_path = '/dev/tty'
+        self.stderr_path = '/dev/tty'
+        self.pidfile_path =  '/tmp/foo.pid'
+        self.pidfile_timeout = 5
+    def run(self):
+        while True:
+		    new_events = app.parse()
+		
+		    if len(new_events) > 0:
+		        body = "The following hackathons have been recently posted:<br><br><ul>"
+		        for event in new_events:
+		            body = body + "<li><a href='" + event[3] + "'><b>" + event[0] + "</b></a> in " + event[2] + " on " + event[1] + "</li>"
+		        body = body + "</ul>"
+		
+		        sendEmail(body)
+		    # else:
+		        # print("Everything is Up to Date")
+		    
+		    deletePYCFiles()
+		    time.sleep(3600)
+
+
+application = App()
+daemon_runner = runner.DaemonRunner(application)
+daemon_runner.do_action()
